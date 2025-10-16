50cdcd78d5d4459867a8d2b3e555ab225c604e4f
Luka Forder <lukaforder@gmail.com>
2025-10-16 18:13:12 +0100

    fix: button colors

--- src/lib/components/ui/button/button.svelte
+++ src/lib/components/ui/button/button.svelte
@@ -8,7 +8,7 @@
 		variants: {
 			variant: {
 				default:
-					'bg-secondary border border-border text-secondary-foreground shadow-xs hover:bg-primary/90',
+					'bg-primary border border-border text-primary-foreground shadow-xs hover:bg-primary/90',
 				destructive:
 					'bg-destructive shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60 text-white',
 				outline:
