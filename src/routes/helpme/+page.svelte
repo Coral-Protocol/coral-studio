6fd909c3e98251f640d140d8f0d9e6ec5c35afe3
Luka Forder <lukaforder@gmail.com>
2025-10-29 16:41:08 +0000

    feat: new helpme route that can be referened in certain  errors

---
+++ src/routes/helpme/+page.svelte
@@ -0,0 +1,7 @@
+<p class="m-auto">
+	Unsure whats wrong? Here's the Coral Protocol discord: <a
+		class="underline"
+		href="https://discord.gg/PYJY5Jrb"
+		target="_blank ">https://discord.gg/PYJY5Jrb</a
+	>
+</p>
