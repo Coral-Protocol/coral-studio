ed78f13ae90c3f38356c2bef53d428e9ca7732bd
Luka Forder <lukaforder@gmail.com>
2025-12-16 16:44:53 +0000

    fix: nested routes in an app group

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,57 +1,9 @@
 <script lang="ts">
 	import { Toaster } from '$lib/components/ui/sonner';
-	import { socketCtx, UserInput } from '$lib/socket.svelte';
 	import { ModeWatcher } from 'mode-watcher';
 	import '../app.css';
-	import { AgentLogs, logContext } from '$lib/logs.svelte';
-	import { watch } from 'runed';
-	import * as Sidebar from '$lib/components/ui/sidebar';
-	import AppSidebar from '$lib/components/app-sidebar.svelte';
-	import { page } from '$app/state';
-	import createClient from 'openapi-fetch';
-	import type { paths } from '$generated/api';
-	import { appContext, type AppContext } from '$lib/context';
-	import { base } from '$app/paths';
-	import { CoralServer } from '$lib/CoralServer.svelte';
 
 	let { children } = $props();
-
-	let ctx: AppContext = $state({
-		server: new CoralServer(),
-		connection: null,
-		session: null,
-		sessions: null,
-		registry: null
-	});
-	appContext.set(ctx);
-
-	let logCtx: ReturnType<(typeof logContext)['get']> = $state({
-		session: null,
-		logs: {}
-	});
-	logContext.set(logCtx);
-
-	//if we get problems we just need to logCtx.logs[agent]?.close() before assigning, according to our good developer friend, Alan.
-
-	watch([() => ctx.session, () => Object.keys(ctx.session?.agents ?? {})], () => {
-		if (!ctx.session) return;
-		if (logCtx.session !== null && logCtx.session !== ctx.session.session) {
-			logCtx.logs = {};
-			console.log('invalidating session logs');
-		}
-		logCtx.session = ctx.session.session;
-		for (const agent of Object.keys(ctx.session.agents)) {
-			if (!(agent in logCtx.logs)) {
-				logCtx.logs[agent] = new AgentLogs({ session: ctx.session.session }, agent);
-				console.log(`opening agent logs for '${agent}'`);
-			}
-		}
-	});
-
-	let socket = $state({
-		userInput: new UserInput()
-	});
-	socketCtx.set(socket);
 </script>
 
 <svelte:head>
@@ -60,9 +12,5 @@
 
 <ModeWatcher />
 <Toaster />
-<Sidebar.Provider>
-	<AppSidebar />
-	<Sidebar.Inset>
-		{@render children()}
-	</Sidebar.Inset>
-</Sidebar.Provider>
+
+{@render children()}
