d6c04e375114f59e451faace18d66dd6cd05e951
Luka Forder <lukaforder@gmail.com>
2025-12-18 16:21:47 +0000

    feat: new tooltip params for more detailed tooltips

--- src/lib/components/tooltip-label.svelte
+++ src/lib/components/tooltip-label.svelte
@@ -5,22 +5,41 @@
 
 	const {
 		tooltip,
+		type,
+		required,
+		title,
 		class: className,
 		children: labelChild
-	}: { tooltip?: string; class?: string; children?: Snippet } = $props();
+	}: {
+		tooltip?: string;
+		type?: string;
+		title?: string;
+		required?: boolean;
+		class?: string;
+		children?: Snippet;
+	} = $props();
 </script>
 
 <Tooltip.Provider>
 	<Tooltip.Root disabled={!tooltip} delayDuration={100}>
 		<Tooltip.Trigger>
 			{#snippet child({ props })}
-				<Label {...props} class="{className} w-fit">
+				<Label {...props} class="{className} !m-0 w-fit">
 					{@render labelChild?.()}
 				</Label>
 			{/snippet}
 		</Tooltip.Trigger>
-		<Tooltip.Content class="w-fit max-w-96 wrap-break-word">
+		<Tooltip.Content class="flex w-fit max-w-96 flex-col gap-2 wrap-break-word ">
+			{#if title}
+				<p class="font-bold">{title}</p>
+			{/if}
 			<p>{tooltip}</p>
+			{#if type}
+				<p><span class="font-bold">type:</span> {type}</p>
+			{/if}
+			{#if required}
+				<p class="font-bold">required</p>
+			{/if}
 		</Tooltip.Content>
 	</Tooltip.Root>
 </Tooltip.Provider>
