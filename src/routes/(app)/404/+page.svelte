0180453364f6d16794c079cf313a7071075f452a
Alan Panayotov <alanpanayotov@gmail.com>
2025-12-20 16:13:36 +0000

    feat: 404 page

---
+++ src/routes/(app)/404/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<main class="flex grow flex-col items-center justify-center">
+	<h1 class="text-5xl font-bold">404</h1>
+	<p class="text-xl">Page not found.</p>
+</main>
