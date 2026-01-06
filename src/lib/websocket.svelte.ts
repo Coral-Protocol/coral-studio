234595db45c7e293981cd5d71c13c87ca0520cd8
Alan Panayotov <alanpanayotov@gmail.com>
2026-01-06 16:06:24 +0000

    fix: PUBLIC_API_PATH for ws

--- src/lib/websocket.svelte.ts
+++ src/lib/websocket.svelte.ts
@@ -3,7 +3,13 @@ import { PUBLIC_API_PATH } from '$env/static/public';
 
 export const createWebsocket = (path: `ws/${string}`) => {
 	if (!browser) return null;
-	return new WebSocket(
-		`${window.location.protocol == 'https:' ? 'wss' : 'ws'}://${window.location.host}${PUBLIC_API_PATH}${path}`
-	);
+	let url = PUBLIC_API_PATH;
+	if (url[0] === '/') {
+		url = `${window.location.protocol == 'https:' ? 'wss' : 'ws'}://${window.location.host}${url}`;
+	} else if (url[0] === 'h') {
+		url = url.replace(/^http/, 'ws');
+	} else {
+		console.error('Bad PUBLIC_API_PATH!', { path: PUBLIC_API_PATH });
+	}
+	return new WebSocket(url + path);
 };
