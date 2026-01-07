91f61bb0940f685758d457f516e2736be0fde37d
Alan Panayotov <alanpanayotov@gmail.com>
2026-01-07 13:56:11 +0000

    feat: naive non-cached logs

--- src/lib/logs.svelte.ts
+++ src/lib/logs.svelte.ts
@@ -1,15 +1,15 @@
-import { base } from '$app/paths';
+import type { components } from '$generated/api';
+import { createWebsocket } from './websocket.svelte';
 
-export type Log = {
-	timestamp: number;
-	kind: any;
-	message: string;
-};
+export type Log = components['schemas']['LoggingEvent'];
+
 export class Logs {
-	// private socket: WebSocket;
-	public connected = $state(false);
+	private socket: WebSocket;
+	public readonly session: string;
 
 	public logs: Log[] = $state([]);
+
+	public state: 'connecting' | 'connected' | 'closed' = $state('connecting');
 
 	constructor(
 		{
@@ -19,30 +19,31 @@ export class Logs {
 		},
 		agentId: string
 	) {
-		// this.socket = new WebSocket(
-		// 	`${base}/ws/v1/debug/` // FIXME: oub???
-		// );
-		// this.socket.onopen = () => {
-		// 	this.connected = true;
-		// };
-		// this.socket.onerror = () => {
-		// 	this.connected = false;
-		// 	this.socket.close();
-		// };
-		// this.socket.onclose = (e) => {
-		// 	this.logs = [];
-		// 	this.connected = false;
-		// };
-		// this.socket.onmessage = (ev) => {
-		// 	let data: Log | null = null;
-		// 	try {
-		// 		data = JSON.parse(ev.data);
-		// 	} catch (e) {
-		// 		console.log('??', e);
-		// 		return;
-		// 	}
-		//
-		// 	data && this.logs.push(data);
-		// };
+		this.session = session;
+		const sock = createWebsocket(`ws/v1/logs?sessionFilter=${this.session}&agentFilter=${agentId}`);
+		if (!sock) throw new Error('cannot create Logs in SSR');
+		this.socket = sock;
+		this.socket.onopen = () => {
+			this.state = 'connected';
+		};
+		this.socket.onerror = () => {
+			this.state = 'closed';
+			this.socket.close();
+		};
+		this.socket.onclose = (e) => {
+			this.logs = [];
+			this.state = 'closed';
+		};
+		this.socket.onmessage = (ev) => {
+			let data;
+			try {
+				data = JSON.parse(ev.data) as components['schemas']['LoggingEvent'];
+			} catch (e) {
+				console.log('??', e);
+				return;
+			}
+
+			data && this.logs.push(data);
+		};
 	}
 }
