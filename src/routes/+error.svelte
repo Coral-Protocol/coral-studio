90ffc5eae7f755e3dd82705b8478b18a4d04b4e2
Luka Forder <lukaforder@gmail.com>
2026-01-30 16:31:13 +0000

    feat: new error handling comp

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,35 +1,35 @@
 <script lang="ts">
-	import { page } from '$app/state';
-	import Button from '$lib/components/ui/button/button.svelte';
-	import * as Alert from '$lib/components/ui/alert';
-	import IconAlertCircle from 'phosphor-icons-svelte/IconWarningCircleRegular.svelte';
-	import Spinner from '$lib/components/ui/spinner/spinner.svelte';
+	import * as Sidebar from '$lib/components/ui/sidebar';
+	import AppSidebar from '$lib/components/app-sidebar.svelte';
+	import { socketCtx, UserInput } from '$lib/socket.svelte';
+	import { appContext, type AppContext } from '$lib/context';
+	import { CoralServer } from '$lib/CoralServer.svelte';
+	import { onMount } from 'svelte';
+	import ErrorPageHandler from '$lib/components/error-page-handler.svelte';
 
-	let reload = $state(false);
+	let ctx: AppContext = $state({
+		server: new CoralServer(),
+		connection: null,
+		session: null,
+		sessions: null,
+		registry: null,
+		logs: null
+	});
+	appContext.set(ctx);
+
+	onMount(() => {
+		ctx.server.fetchAll();
+	});
+
+	let socket = $state({
+		userInput: new UserInput()
+	});
+	socketCtx.set(socket);
 </script>
 
-<main
-	class="fixed top-1/2 left-1/2 flex w-1/2 -translate-x-1/2 -translate-y-1/2 flex-col items-center gap-4"
->
-	<Alert.Root>
-		<IconAlertCircle />
-		<Alert.Title>Oops! Something went wrong.</Alert.Title>
-		<Alert.Description>
-			<h1>Status: <span class="text-destructive">{page.status}</span></h1>
-			<p>Error: <span class="text-destructive">{page.error?.message}</span></p>
-		</Alert.Description>
-	</Alert.Root>
-
-	<ol class="mt-4 flex gap-2">
-		<Button href="/">Go Home</Button>
-		<Button onclick={() => window.history.back()}>Go Back</Button>
-		<Button onclick={() => (window.location.reload(), (reload = true))} class="relative">
-			<Spinner
-				class="absolute top-1/2 left-1/2 flex w-1/2 -translate-x-1/2 -translate-y-1/2 {reload
-					? 'opacity-100'
-					: 'opacity-0'}"
-			/>
-			<span class={reload ? 'opacity-0' : 'opacity-100'}>Retry</span>
-		</Button>
-	</ol>
-</main>
+<Sidebar.Provider>
+	<AppSidebar />
+	<Sidebar.Inset class="relative">
+		<ErrorPageHandler />
+	</Sidebar.Inset>
+</Sidebar.Provider>
