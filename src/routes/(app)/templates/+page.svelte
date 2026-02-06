9b69f50b205c764feff4b338d095b32f6ed129b6
Luka Forder <lukaforder@gmail.com>
2026-02-06 16:18:36 +0000

    feat: new templates

---
+++ src/routes/(app)/templates/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+</script>
+
+here is all our saved templates create new template?
+
+<Button variant="link" href="/templates/create">Yes</Button>
