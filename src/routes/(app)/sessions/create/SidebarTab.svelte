689e80176d940c3ca8dd7ce34a11aa0c38af8c3c
Alan Panayotov <alanpanayotov@gmail.com>
2026-02-06 17:14:14 +0000

    feat: sidebar tab improvements

---
+++ src/routes/(app)/sessions/create/SidebarTab.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import * as Tabs from '$lib/components/ui/tabs';
+	import { cn } from '$lib/utils';
+	import type { TabsTriggerProps } from 'bits-ui';
+	import type { Component } from 'svelte';
+
+	let {
+		class: className,
+		icon: Icon,
+		invalid = false,
+		children,
+		...rest
+	}: TabsTriggerProps & { icon: Component; invalid?: boolean } = $props();
+</script>
+
+<Tabs.Trigger class={cn('group @container flex items-center gap-1 truncate', className)} {...rest}>
+	<Icon class={cn('size-6', invalid && 'group-not-data-[state=active]:text-destructive')} />
+	<span
+		class={cn('hidden @[11ch]:inline', invalid && 'group-not-data-[state=active]:text-destructive')}
+	>
+		{@render children?.()}
+	</span>
+</Tabs.Trigger>
