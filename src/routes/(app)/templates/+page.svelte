2b1cf294c89b7cba5e8ed80446fb8c5c676a1e00
Luka Forder <lukaforder@gmail.com>
2026-02-11 14:38:14 +0000

    Revert "feat: new templates"
    This reverts commit 9b69f50b205c764feff4b338d095b32f6ed129b6.

--- src/routes/(app)/templates/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import Button from '$lib/components/ui/button/button.svelte';
-</script>
-
-here is all our saved templates create new template?
-
-<Button variant="link" href="/templates/create">Yes</Button>
